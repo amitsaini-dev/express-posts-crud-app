41d4322cd37eb6cf0d4a04fc910d2600fad0de2e
Amit Saini <amitsaini.ce@gmail.com>
2025-12-07 22:03:51 +0530

    Added edit post functionality using EJS form and method-override (PATCH request support)

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+var methodOverride = require('method-override')
+
 
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public/css")));
 app.use(express.static(path.join(__dirname, "public/js")));
+app.use(methodOverride('_method'))
 
 let posts = [
     {
@@ -51,6 +54,21 @@ app.get("/posts/:id", (req, res) => {
     }
     let post = posts.find((p) => id === p.id);
     res.render("show.ejs", { post });
+})
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", { post });
 })
 
 app.listen(port, () => {
