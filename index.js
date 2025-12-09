a915d4e348caaff2d61a338c8e993434cef3b8d1
Amit Saini <amitsaini.ce@gmail.com>
2025-12-09 13:16:07 +0530

    Added delete post functionality with method-override and inline delete form

--- index.js
+++ index.js
@@ -71,6 +71,13 @@ app.get("/posts/:id/edit", (req, res) => {
     res.render("edit.ejs", { post });
 })
 
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+})
+
+
 app.listen(port, () => {
     console.log(`App is listining at port ${port}`);
 })
